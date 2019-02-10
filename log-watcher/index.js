2c319cfe02a0bfb85fccebd84a841e8d15bedecf
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-02-10 22:33:25 +0100

    feat: watch logs to remove files in excess

--- log-watcher/index.js
+++ log-watcher/index.js
@@ -1,13 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 
-const logPath = path.resolve(__dirname, '../logs/');
+// only save up to 100 files
+const MAX_FILES = 100;
+const logs = path.resolve(__dirname, '../logs/');
 
-console.log(`Watching ${logPath}`);
+const getLogs = async () => {
+  return new Promise(resolve =>
+    fs.readdir(logs, (err, files = []) => {
+      if (err) return resolve([]);
+      return resolve(files);
+    }),
+  );
+};
 
-fs.watch(`${logPath}`, (eventType, filename) => {
-  console.log(eventType);
+console.log(`Watching ${logs}`);
+
+fs.watch(`${logs}`, async (eventType, filename) => {
   // could be either 'rename' or 'change'. new file event and delete
   // also generally emit 'rename'
-  console.log(filename);
+
+  const files = await getLogs();
+  if (files.length > MAX_FILES) {
+    const [fileToDelete] = files.sort((a, b) => {
+      const [unixA] = a.split('.');
+      const [unixB] = b.split('.');
+      return parseInt(unixA) - parseInt(unixB);
+    });
+
+    // path of deletion target
+    const pathToDelete = `${logs}/${fileToDelete}`;
+    fs.unlink(pathToDelete, async () => {
+      const updated = await getLogs();
+      console.log(`Deleted ${fileToDelete}. Got ${updated.length} logs left.`);
+    });
+  }
+  return null;
 });
